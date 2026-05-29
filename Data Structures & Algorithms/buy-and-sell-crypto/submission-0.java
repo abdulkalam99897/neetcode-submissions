39a10f00ddfd7188bf7702efc31fbba5f8139bc3
abdulkalam99897 <abdulabdul77606@gmail.com>
2026-05-29 13:13:36 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int bp = prices[0];
+        int maxprofit = 0;
+
+        for(int i = 0; i < prices.length;i++){
+            if(bp < prices[i]){
+                int profit = prices[i] - bp;
+                maxprofit = Math.max(maxprofit,profit);
+            }
+            else{
+                bp = prices[i];
+            }
+        }
+    return maxprofit;
+    }
+}
